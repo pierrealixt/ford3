068e2cff7ea45cbcc404c11437a72d0475f70dd8
pierrealixt <pierrealix@icloud.com>
2019-03-27 13:08:35 +0200

    remove Address model; add street_name, city and postal code to physical and postal address

--- django_project/ford3/models/campus.py
+++ django_project/ford3/models/campus.py
@@ -1,7 +1,6 @@
 from django.contrib.gis.db import models
 
 from ford3.models.provider import Provider
-from ford3.models.address import Address
 
 
 class Campus(models.Model):
@@ -41,19 +40,47 @@ class Campus(models.Model):
         null=True,
         unique=False,
         help_text='')
-    physical_address = models.ForeignKey(
-        Address,
-        on_delete=models.CASCADE,
-        related_name='physical_address',
-        blank=True,
-        null=True)
+    physical_address_street_name = models.CharField(
+        blank=False,
+        null=True,
+        unique=False,
+        help_text='',
+        max_length=255)
 
-    postal_address = models.ForeignKey(
-        Address,
-        on_delete=models.CASCADE,
-        related_name='postal_address',
-        blank=True,
-        null=True)
+    physical_address_city = models.CharField(
+        blank=False,
+        null=True,
+        unique=False,
+        help_text='',
+        max_length=255)
+
+    physical_address_postal_code = models.CharField(
+        blank=False,
+        null=True,
+        unique=False,
+        help_text='',
+        max_length=255)
+
+    postal_address_street_name = models.CharField(
+        blank=False,
+        null=True,
+        unique=False,
+        help_text='',
+        max_length=255)
+
+    postal_address_city = models.CharField(
+        blank=False,
+        null=True,
+        unique=False,
+        help_text='',
+        max_length=255)
+
+    postal_address_postal_code = models.CharField(
+        blank=False,
+        null=True,
+        unique=False,
+        help_text='',
+        max_length=255)
 
     pass
 
