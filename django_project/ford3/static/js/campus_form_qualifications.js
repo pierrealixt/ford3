114932fb3c5ec4b1c87e2d7fbe32c7658f67cbe5
pierrealixt <pierrealix@icloud.com>
2019-03-28 20:02:08 +0200

    campus form: search saqa qualifications

--- django_project/ford3/static/js/campus_form_qualifications.js
+++ django_project/ford3/static/js/campus_form_qualifications.js
@@ -11,6 +11,10 @@ const getRemoveQualificationButtonElement = () => {
   return document.querySelector('button[data-action="remove-qualif"]')
 }
 
+const getClearQualificationsButtonElement = () => {
+  return document.querySelector('button[data-action="clear-qualifs"]')
+}
+
 const getCampusQualificationsListElement = () => {
   return document.getElementById('campus-qualifications-list')
 }
@@ -41,7 +45,7 @@ const removeSaqaQualification = (saqaId) => {
 
 const setToggleEvent = (elem) => {
   elem.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('selected')
+    if (evt.target.tagName !== 'A') { evt.target.classList.toggle('selected') }
   })
 }
 
@@ -86,14 +90,13 @@ const setClickEventToRemoveButton = () => {
   var removeQualifButton = getRemoveQualificationButtonElement()
   removeQualifButton.addEventListener('click', function (evt) {
     evt.stopPropagation()
-    var saqaQualifListElem = getSaqaQualificationsListElement()
-    var campusQualifListElem = getCampusQualificationsListElement()
+    const saqaQualifListElem = getSaqaQualificationsListElement()
+    const campusQualifListElem = getCampusQualificationsListElement()
     var selectedQualifElems = getSelectedQualificationsFromList(campusQualifListElem)
 
     selectedQualifElems.forEach(function (selectedQualifElem) {
       var qualifElem = saqaQualifListElem.querySelector('li[data-saqa-id="' + selectedQualifElem.dataset['saqa-id'] + '"]')
-
-      qualifElem.style.display = 'list-item'
+      if (qualifElem) { qualifElem.style.display = 'list-item' }
 
       campusQualifListElem.removeChild(selectedQualifElem)
 
@@ -103,21 +106,96 @@ const setClickEventToRemoveButton = () => {
   })
 }
 
+const setClickEventToClearButton = () => {
+  const button = getClearQualificationsButtonElement()
+  const input = getSearchQualifInputElem()
+
+  button.addEventListener('click', function (evt) {
+    evt.stopPropagation()
+
+    input.value = ''
+
+    const saqaQualifListElem = getSaqaQualificationsListElement()
+    saqaQualifListElem.querySelectorAll('li').forEach(function (li) {
+      saqaQualifListElem.removeChild(li)
+    })
+  })
+}
+const getSearchQualifInputElem = () => {
+  return document.querySelector('input[data-action="search-qualif"]')
+}
+
+const buildSaqaQualificationLiContent = (saqa) => {
+  let linkText = document.createTextNode('(' + saqa.saqa_id + ')')
+  let link = document.createElement('a')
+  link.href = 'http://regqs.saqa.org.za/viewQualification.php?id=' + saqa.saqa_id
+  link.target = '_blank'
+  link.appendChild(linkText)
+  return link
+}
+
+const displaySaqaQualificationsResults = (results) => {
+  const list = getSaqaQualificationsListElement()
+  list.querySelectorAll('li').forEach(function (li) {
+    list.removeChild(li)
+  })
+
+  results.forEach(function (saqa) {
+    let saqaNode = document.createElement('li')
+    saqaNode.setAttribute('data-saqa-id', saqa.saqa_id)
+    const saqaNodeContent = document.createTextNode(saqa.name)
+    const saqaLink = buildSaqaQualificationLiContent(saqa)
+    saqaNode.appendChild(saqaLink)
+    saqaNode.appendChild(saqaNodeContent)
+
+    list.appendChild(saqaNode)
+
+    setToggleEvent(saqaNode)
+  })
+}
+
+const ajaxSearchQualifications = (query) => {
+  const url = '/ford3/saqa_qualifications?q=' + query
+  const request = new XMLHttpRequest()
+  request.open('GET', url, true)
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+    // Success!
+      var data = JSON.parse(request.responseText)
+      displaySaqaQualificationsResults(data['results'])
+    } else {
+    // We reached our target server, but it returned an error
+
+    }
+  }
+
+  request.onerror = function () {
+  // There was a connection error of some sort
+  }
+
+  request.send()
+}
+
+const setSearchEvent = () => {
+  let timeout = null
+  const elem = getSearchQualifInputElem()
+  elem.onkeyup = function (e) {
+    clearTimeout(timeout)
+
+    timeout = setTimeout(function () {
+      if (elem.value.length > 3) { ajaxSearchQualifications(elem.value) }
+    }, 842)
+  }
+}
+
 const setupEvents = () => {
   setClickEventToLi()
   setClickEventToAddButton()
   setClickEventToRemoveButton()
+  setClickEventToClearButton()
+  setSearchEvent()
 }
-
-// var searchQualifElem = document.getElementById('search_qualif')
-// var timeout = null
-// searchQualifElem.onkeyup = (function (e) {
-//   clearTimeout(timeout)
-
-//   timeout = setTimeout(function () {
-//     console.log('Input Value:', searchQualifElem.value)
-//   }, 500)
-// }
 
 (function () {
   setupEvents()
