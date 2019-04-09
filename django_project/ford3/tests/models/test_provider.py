ab50630c43bafd7124484a812d2cb3f212656fec
pierrealixt <pierrealix@icloud.com>
2019-04-09 11:23:22 +0200

    redirect to the edit form if its a new provider

--- django_project/ford3/tests/models/test_provider.py
+++ django_project/ford3/tests/models/test_provider.py
@@ -12,6 +12,17 @@ class TestProvider(TestCase):
 
         self.assertEqual(str(self.new_provider), 'Object Test Name')
 
+    def test_is_new_provider(self):
+        self.assertTrue(self.new_provider.is_new_provider)
+
+        campus = ModelFactories.get_campus_test_object()
+        campus.provider_id = self.new_provider.id
+        campus.save()
+
+        self.assertEqual(self.new_provider.id, campus.provider_id)
+
+        self.assertFalse(self.new_provider.is_new_provider)
+
     def test_correct_GET_template_used(self):
         url = reverse(
                 'edit-provider',
