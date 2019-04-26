34e56858a6a7cc5d05df374046794fb30a3c7e3e
pierrealixt <pierrealix@icloud.com>
2019-04-26 06:58:29 +0200

    added login/logout routes

--- django_project/ford3/urls.py
+++ django_project/ford3/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from ford3.views import views
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from ford3.forms.qualification import (
     QualificationDetailForm,
     QualificationDurationFeesForm,
@@ -72,4 +73,6 @@ urlpatterns = [
         views.show_qualification,
         name='show-qualification'),
     url(r'^test_widgets/$', views.widget_examples, name='test_widgets'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
 ]
