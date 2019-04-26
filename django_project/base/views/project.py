5ed05ab16edab2e870b39a5e079391e46d23a547
pierrealixt <pierrealix@icloud.com>
2019-04-26 21:27:21 +0200

    show Prospect form on the landing page

--- django_project/base/views/project.py
+++ django_project/base/views/project.py
@@ -6,6 +6,12 @@ logger = logging.getLogger(__name__)
 
 from django.views.generic import TemplateView
 
+from ford3.forms.prospect_form import ProspectForm
 
 class Home(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['prospect_form'] = ProspectForm()
+        return context
