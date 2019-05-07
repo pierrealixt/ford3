dcc820f56c7bb6a17e46d032061e3316dc78e3b1
pierrealixt <pierrealix@icloud.com>
2019-05-07 09:46:31 +0200

    remove comments

--- django_project/ford3/views/provider.py
+++ django_project/ford3/views/provider.py
@@ -123,26 +123,6 @@ def show_provider(request, provider_id):
             args=[str(provider.id)])
         return redirect(redirect_url)
 
-    # context = {}
-    # form_data = {}
-    # campus_query = Campus.objects.filter(provider__id=provider_id).annotate(
-    #     campus_name=F('name'),
-    #     campus_id=F('id'),
-    #     provider_name=F('provider__name'),
-    #     provider_logo=F('provider__provider_logo')
-    # )
-    # campus_data = campus_query.values('name', 'id')
-    
-
-    # form_data['campus_list'] = list(campus_data)
-    # form_data['provider_name'] = str(provider_name)
-
-    # context['form_data'] = form_data
-    # context['provider'] = {
-    #     'campus': campus_data,
-    #     'id': provider_id,
-    #     'name': provider_name,
-    # }
     context = {
         'provider': provider
     }
