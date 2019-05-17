12fb108e849c1e22f307c64b7141ff634a7dda69
pierrealixt <pierrealix@icloud.com>
2019-05-17 10:27:07 +0200

    configure LOGGING for rancher

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -41,3 +41,19 @@ EMAIL_HOST_USER = 'noreply@kartoza.com'
 EMAIL_HOST_PASSWORD = 'docker'
 EMAIL_USE_TLS = False
 EMAIL_SUBJECT_PREFIX = '[FORD3]'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
