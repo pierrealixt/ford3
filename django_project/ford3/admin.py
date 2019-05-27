4cb1eeebb5ca1ecd421ee26b1722673def243a39
pierrealixt <pierrealix@icloud.com>
2019-05-27 15:33:01 +0200

    Create a provincial user from the admin

--- django_project/ford3/admin.py
+++ django_project/ford3/admin.py
@@ -47,6 +47,31 @@ class UserAdmin(admin.ModelAdmin):
     form = UserAdminForm
 
 
+class ProvinceUser(User):
+    class Meta:
+        proxy=True
+    
+    def save(self, *args, **kwargs):
+        self.is_province = True
+        self.username = self.email
+        super().save(*args, **kwargs)
+
+
+class ProvinceUserAdminForm(forms.ModelForm):
+    class Meta:
+        model = ProvinceUser
+        fields = [
+            'email',
+        ]
+
+class ProvinceUserAdminForm(admin.ModelAdmin):
+    form = ProvinceUserAdminForm
+    def get_queryset(self, request):
+        return self.model.objects.filter(is_province=True)
+
+
+
+admin.site.register(ProvinceUser, ProvinceUserAdminForm)
 admin.site.register(User, UserAdmin)
 
 
