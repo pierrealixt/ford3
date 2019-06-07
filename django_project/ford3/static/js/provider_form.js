b7a38d545656ce1d3b3ac87d3fa983fa80f0d396
pierrealixt <pierrealix@icloud.com>
2019-06-07 10:06:53 +0200

    fixed provider postal address being grayed out when editing

--- django_project/ford3/static/js/provider_form.js
+++ django_project/ford3/static/js/provider_form.js
@@ -66,10 +66,16 @@ function setPostalAddressEnabled () {
 }
 
 function disablePostalAddress () {
+  let postal_address_does_differ = document.getElementById('id_postal_address_differs').checked
   let postal_address_container = document.getElementById('postal_address_container')
-  postal_address_container.style.pointerEvents = 'none'
-  postal_address_container.style.opacity = '0.4'
-  $(':input', '#postal_address_container').each(function () {
-    this.required = false
-  })
+
+  if (postal_address_does_differ) {
+    setPostalAddressEnabled()
+  } else {
+    postal_address_container.style.pointerEvents = 'none'
+    postal_address_container.style.opacity = '0.4'
+    $(':input', '#postal_address_container').each(function () {
+      this.required = false
+    })
+  }
 }
