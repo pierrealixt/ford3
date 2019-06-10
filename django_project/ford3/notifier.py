06ddc3ab0b80aec8748a21d2c362997d718025ad
pierrealixt <35932320+pierrealixt@users.noreply.github.com>
2019-06-10 16:59:30 +0200

    use account_activation_token (#556)

--- django_project/ford3/notifier.py
+++ django_project/ford3/notifier.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
-# from django.contrib.auth.tokens import default_token_generator
 from django.template.loader import render_to_string
 from ford3.tokens import account_activation_token
 
