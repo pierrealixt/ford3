96c2d6120b853e9479cdb62ab907e7981330df07
Christiaan van der Merwe <Christiaan@Kartoza.com>
2019-06-10 09:51:44 +0200

    Update SAQA API response (ford3-526)

--- django_project/api/viewsets/saqa_qualification.py
+++ django_project/api/viewsets/saqa_qualification.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from ford3.models.saqa_qualification import SAQAQualification
 from api.serializers.saqa_qualification import SAQAQualificationSerializer
 
@@ -6,3 +7,8 @@ from api.serializers.saqa_qualification import SAQAQualificationSerializer
 class SAQAQualificationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SAQAQualification.objects.all()
     serializer_class = SAQAQualificationSerializer
+
+    def retrieve(self, request, version, pk=None):
+        queryset = SAQAQualification.objects.get(saqa_id=pk)
+        serializer = SAQAQualificationSerializer(queryset)
+        return Response(serializer.data)
