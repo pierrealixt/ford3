40572cc1056817465364ba7f0b3720c16059070a
Christiaan van der Merwe <Christiaan@Kartoza.com>
2019-06-10 18:34:05 +0200

    Removed campus from initial provider form (#554)

--- django_project/ford3/static/js/provider_form.js
+++ django_project/ford3/static/js/provider_form.js
@@ -1,50 +1,6 @@
 $(document).ready(function () {
-  innitiateRemoveCampusButtons()
   disablePostalAddress()
 })
-
-function innitiateRemoveCampusButtons () {
-  $('.remove-campus-button').click(function () {
-    if ($('.campus_name').length < 1) {
-      alert('At least one campus should be filled')
-      return false
-    }
-    this_button_parrent_parent = $(this).parent().parent()
-    this_button_parrent_parent.remove()
-  })
-}
-
-$(document).on('submit', 'form', function (e) {
-  campus_name_elements = $('.campus_name').elements
-  return true
-})
-
-$('#add-campus-name').click(
-  function () {
-    addCampusNameInput()
-    innitiateRemoveCampusButtons()
-  }
-)
-
-function addCampusNameInput () {
-  var campus_container = $('#campus-names-input-wrapper')
-  var result_html = ('<div>' +
-            '<div class="row my-3 campus-name-fade-in">' +
-                '<div class="col-md-4"><label>' +
-                    'Campus Name ' +
-                '</label></div>' +
-                '<div class="col-md-7">' +
-                    '<input name="campus_name" required class="campus_name" ' +
-                    'type="text" placeholder="•••••••••••••••••"/>' +
-                '</div>' +
-                '<div class="col-md-1">' +
-                    '<div class="remove-campus-button">' +
-                    '<div class="remove-campus-button-inner ">X</div></div>' +
-                '</div>' +
-            '</div></div>')
-  let new_input = $.parseHTML(result_html)
-  campus_container.append(new_input)
-}
 
 $('#id_postal_address_differs').click(function (e) {
   setPostalAddressEnabled()
