0024e056003cba1a9779fb8f512e020019ad95e9
Christiaan van der Merwe <Christiaan@Kartoza.com>
2019-06-14 11:14:34 +0200

    Added add_http_to_link when creating new events. (ford3-573)

--- django_project/ford3/views/events.py
+++ django_project/ford3/views/events.py
@@ -53,7 +53,7 @@ def create(request, owner_id, event_type):
     new_event.name = request.POST['name']
     new_event.date_start = request.POST['date_start']
     new_event.date_end = request.POST['date_end']
-    new_event.http_link = request.POST['http_link']
+    new_event.http_link = add_http_to_link(request.POST['http_link'])
 
     try:
         new_event.full_clean()
