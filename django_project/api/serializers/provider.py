6bdca1051ef67a28edb958af87284d1b0ddb5091
Christiaan van der Merwe <Christiaan@Kartoza.com>
2019-06-24 15:47:29 +0200

    Updated province to be a string related field in the API (ford3-600)

--- django_project/api/serializers/provider.py
+++ django_project/api/serializers/provider.py
@@ -7,6 +7,7 @@ from ford3.models.campus import Campus
 
 class ProviderSerializer(serializers.ModelSerializer):
     campus = serializers.SerializerMethodField()
+    province = serializers.StringRelatedField()
 
     def get_campus(self, obj):
         queryset = list(Campus.active_objects.filter(provider_id=obj.id).all())
