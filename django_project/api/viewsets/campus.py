0b71c9c68d6600e8a81f09b7c8e37fdb41757f93
Christiaan van der Merwe <Christiaan@Kartoza.com>
2019-06-24 12:50:37 +0200

    Added equality filtering to API for providers, campus and qualification (ford3-601)

--- django_project/api/viewsets/campus.py
+++ django_project/api/viewsets/campus.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from ford3.models.campus import Campus
 from api.serializers.campus import CampusSerializer
 
@@ -10,10 +11,21 @@ class CampusViewSet(viewsets.ReadOnlyModelViewSet):
 
     list:
     Returns a list of all campuses registered with OpenEdu.
-
     """
-
     serializer_class = CampusSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_fields = (
+        'provider',
+        'name',
+        'telephone',
+        'email',
+        'max_students_per_year',
+        'physical_address_city')
+    ordering_fields = (
+        'name',
+        'id',
+        'max_students_per_year',
+        'physical_address_city')
 
     def get_queryset(self):
         return Campus.active_objects.all()
--- django_project/api/viewsets/qualification.py
+++ django_project/api/viewsets/qualification.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from ford3.models.qualification import Qualification
 from api.serializers.qualification import QualificationSerializer
 
@@ -13,6 +14,24 @@ class QualificationViewSet(viewsets.ReadOnlyModelViewSet):
     Returns a list of all qualifications registered with OpenEdu.
     """
     serializer_class = QualificationSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_fields = (
+        'name',
+        'campus',
+        'saqa_qualification',
+        'short_description',
+        'long_description',
+        'duration',
+        'full_time',
+        'credits_after_completion',
+        'distance_learning',
+        'completion_rate',
+        'total_cost',
+        'critical_skill',
+        'green_occupation',
+        'high_demand_occupation'
+    )
+    ordering_fields = ('name', 'id', 'total_cost')
 
     def get_queryset(self):
         return Qualification.published_objects.all()
