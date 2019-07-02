dcbb087c37be38d9bfed53b3683e5f275f7cc253
pierrealixt <pierrealix@icloud.com>
2019-07-02 11:39:27 +0200

    API: return required subjects with minimum score

--- django_project/api/serializers/requirements.py
+++ django_project/api/serializers/requirements.py
@@ -5,8 +5,15 @@ from api.serializers.utilities.enum_field import EnumField
 
 
 class RequirementSerializer(serializers.ModelSerializer):
+    min_nqf_level = EnumField(enum=SaqaQualificationLevel)
+    subjects = serializers.SerializerMethodField()
+
+    def get_subjects(self, obj):
+        if obj.require_certain_subjects:
+            return obj.qualification.entrance_req_subjects_list
+        else:
+            return []
+
     class Meta:
         model = Requirement
         fields = '__all__'
-
-    min_nqf_level = EnumField(enum=SaqaQualificationLevel)
