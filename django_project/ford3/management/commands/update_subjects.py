95541707f71ce29f856abcb4dfe5b2ba590da6f2
pierrealixt <35932320+pierrealixt@users.noreply.github.com>
2019-07-22 07:19:56 +0200

    Add more subjects (#641)

---
+++ django_project/ford3/management/commands/update_subjects.py
@@ -0,0 +1,43 @@
+import itertools
+from django.core.management.base import BaseCommand
+from django.db.models.deletion import ProtectedError
+from ford3.models.subject import Subject
+from ford3.models.qualification_entrance_requirement_subject import QualificationEntranceRequirementSubject # noqa
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # add languages
+        for lng in ['English', 'Afrikaans', 'IsiNdebele', 'IsiXhosa', 'IsiZulu', 'Sepedi', 'Sesotho', 'Setswana', 'Xitstonga', 'Tshivenda', 'SeSwati']: # noqa
+            if not Subject.objects.filter(name=lng).exists():
+                print(f'Creating {lng}')
+                s = Subject()
+                s.name = lng
+                s.is_language = True
+                s.save()
+
+        # remove "Home language" and "First additional language"
+        for lng in ['Home language', 'First additional language']:
+            try:
+                subject = Subject.objects.get(name=lng)
+                subject.delete()
+                print(f'{lng} deleted')
+            except Subject.DoesNotExist:
+                print(f'{lng} does not exist')
+                pass
+            except ProtectedError:
+                QualificationEntranceRequirementSubject.objects.filter(
+                    subject=subject).delete()
+                subject.delete()
+                print(f'{lng} deleted with dependencies')
+
+        # add "Any other subject" three times
+        subject_name = "Any other subject"
+        if Subject.objects.filter(name=subject_name).count() == 0:
+            for _ in itertools.repeat(None, 3):
+                s = Subject(
+                    name=subject_name,
+                    is_other=True)
+                s.save()
+            assert Subject.objects.filter(name=subject_name).count(), 3
+            print('Added "Any other subject" 3 times')
--- django_project/ford3/models/subject.py
+++ django_project/ford3/models/subject.py
@@ -17,6 +17,16 @@ class Subject(models.Model):
         help_text="A short description of what the learner can expect to "
                   "learn whilst completing this subject",
         max_length=500)
+    is_language = models.BooleanField(
+        blank=True,
+        null=True,
+        default=False,
+        help_text="Is the subject a language?")
+    is_other = models.BooleanField(
+        blank=True,
+        null=True,
+        default=False,
+        help_text="Is the subject any other subject")
 
     def __str__(self):
         return self.name
